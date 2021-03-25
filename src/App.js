321fdd1766fc6f5e73afd9654ce735169bfa63da
Logan Emmert <loganlemmert@gmail.com>
2021-03-25 13:03:22 -0600

    Updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn React AND MORE STUFF
+        Learn React AND MORE STUFF TIMes Tw02
         </a>
       </header>
     </div>
