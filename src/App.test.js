756430225a243d09061066a3303ce67afbb508a5
Logan Emmert <loganlemmert@gmail.com>
2021-04-12 02:27:33 -0600

    added component testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Homepage from './components/pages/Homepage';
+import About from './components/pages/About';
+import Menu from './components/Menu';
+import Title from './components/Title';
+import Footer from './components/Footer';
+import Sample from './components/pages/Samples';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+test('renders Homepage correctly', () => {
+  render(<Homepage />);
+  const linkElement = screen.getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders About Page correctly', () => {
+  render(<About />);
+  const linkElement = screen.getByText(/Results may vary/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Title Banner', () => {
+  render(<Title />);
+  const linkElement = screen.getByText(/Quiz Ninjas/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Menu', () => {
+  render(
+    <BrowserRouter>
+      <Menu/>
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/About Us/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders footer', () => {
+  render( <Footer/>);
+  const linkElement = screen.getByText(/Thank/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('sample page renders', () => {
+  render(<Sample/>);
+  const linkElement = screen.getByText(/Miley Cyrus/i);
   expect(linkElement).toBeInTheDocument();
 });
